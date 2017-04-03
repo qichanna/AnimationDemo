1de68f97c4b7c7ff86ff0a44770bfd0bc7a16354
qichanna <qichanna@126.com>
2017-04-03 19:57:51 +0800

    vector modify

--- app/src/main/java/animationdemo/liqi/com/animationdemo/MainActivity.java
+++ app/src/main/java/animationdemo/liqi/com/animationdemo/MainActivity.java
@@ -2,13 +2,19 @@ package animationdemo.liqi.com.animationdemo;
 
 import android.content.Intent;
 import android.graphics.drawable.Animatable;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +33,14 @@ public class MainActivity extends AppCompatActivity {
     public void toBerzierActivity(View view){
         Intent i = new Intent(this,BerzierActivity.class);
         startActivity(i);
+    }
+
+    public void anim_five(View view){
+        ImageView imageView = (ImageView) view;
+        AnimatedVectorDrawable drawable = (AnimatedVectorDrawable) getDrawable(R.drawable.fivestar_anim);
+        imageView.setImageDrawable(drawable);
+        if(drawable != null){
+            drawable.start();
+        }
     }
 }
